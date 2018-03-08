610562d8b353b45ef3c722076f161e442a7f8833
wangkaixin <491045545@qq.com>
2018-03-08 14:41:30 +0800

    add new homework

---
+++ lesson1/myFiveSides.js
@@ -0,0 +1,11 @@
+(function () {
+    CanvasRenderingContext2D.prototype.fiveSides = function (x1,y1,x2,y2,x3,y3,x4,y4,x5,y5) {
+        this.moveTo(x1,y1);
+        this.lineTo(x2,y3);
+        this.lineTo(x3,y3);
+        this.lineTo(x4,y4);
+        this.lineTo(x5,y5);
+        this.closePath();
+        this.stroke();
+    }
+})();
